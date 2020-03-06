0b46f209cce47452af04e3066f4ca689b4c37ff8
Julian Ding <scorpiusa51@gmail.com>
2020-03-06 09:49:21 -0800

    Add avoidence of self-loops
    Still need avoidence of making loops with walls

--- app/utils/arena.py
+++ app/utils/arena.py
@@ -32,6 +32,16 @@ TURN_DICT = {
     (DN, LT): CW,
     (LT, UP): CW
 }
+TURN_TO_DIRECTION = {
+    (UP, CCW): LT,
+    (LT, CCW): DN,
+    (DN, CCW): RT,
+    (RT, CCW): UP,
+    (UP, CW): RT,
+    (RT, CW): DN,
+    (DN, CW): LT,
+    (LT, CW): UP
+}
 
 
 # tweakable parameters
@@ -40,6 +50,7 @@ DEATH = 50.0
 HILLTOP = -DEATH # Value for all points from which to propagate hills
 DANGER = 10.0
 FOOD = -5.0 # Value for all points from which to propogate wells
+FORCED_DECISION = -20.0 # For when you really want the snake to choose a particular direction
 DECAYFACTOR_A = 1.0
 DECAYFACTOR_B = 0.4
 
@@ -70,6 +81,8 @@ class Arena(object):
         diff = (head[0]-prev[0], head[1]-prev[1])
         self.directions.append(DIR_DICT[diff])
         turn = tuple(self.directions[-2:])
+        if len(turn) < 2:
+            turn = (turn[0], turn[0])
         self.turn_state += TURN_DICT[turn]
 
 
@@ -182,20 +195,82 @@ class Arena(object):
         Parameters:
         move -- one of UP, DN, LT, RT
         '''
+        # Prevent running into walls
+        if self.run_into_wall(move):
+            return False
+        # If moving toward food, mark tail as certain death
         dx, dy = MOVE_DICT[move]
         hx, hy = self.body[0]
         nx = hx+dx
         ny = hy+dy
-        # Prevent running into walls
-        width = self._position_grid.shape[0]
-        height = self._position_grid.shape[1]
-        if nx < 0 or ny < 0 or nx >= width or ny >= height:
-            return False
-        # If moving toward food, mark tail as certain death
         if (nx, ny) in self.foods:
             tx, ty = self.body[-1]
             self._position_grid[tx][ty] = DEATH
+        
         return self._position_grid[nx][ny] < DEATH
+
+    
+    def check_self_loop(self):
+        '''
+        Checks if snake is about to loop in on itself
+        For now, marks self-looping as DEATH
+        '''
+        turn_state = self.turn_state
+        curr_dir = self.directions[0]
+        hx, hy = self.body[0]
+        fx, fy = MOVE_DICT[curr_dir]
+        # Case 1: Looping in on own body
+        if abs(turn_state) > 2:
+            # Check three blocks perpendicular to head
+            if fx == 0:
+                # Direction is up/down, draw danger zone left/right
+                ahead = [(hx-1, hy+fy), (hx, hy+fy), (hx+1, hy+fy)]
+            else:
+                # Direction is left/right, draw danger zone up/down
+                ahead = [(hx+fx, hy-1), (hx+fx, hy), (hx+fx, hy+1)]
+            # In the case of a self-loop...
+            if any([pos in self.body for pos in ahead]):
+                turn_away = CW if turn_state > 0 else CCW
+                dx, dy = MOVE_DICT[TURN_TO_DIRECTION[(curr_dir, turn_away)]]
+                # If outward turn is legal, reward outward turn
+                nx = hx+dx
+                ny = hy+dy
+                next_pos_value = self._position_grid[nx][ny]
+                if next_pos_value < DEATH:
+                    self._position_grid[nx][ny] = FORCED_DECISION
+        # Case 2: Forming loop with arena boundaries
+        if self.run_into_wall(curr_dir) and any([on_walls(seg) > 0 for seg in self.body[3:]]):
+            # TODO: Choose direction corresponding to greater available play area
+            pass
+
+    def run_into_wall(self, move):
+        '''Checks if move will result in collision with arena boundaries
+
+        Parameters:
+        move -- one of UP, DN, LT, RT
+        '''
+        dx, dy = MOVE_DICT[move]
+        hx, hy = self.body[0]
+        nx = hx+dx
+        ny = hy+dy
+        width, height = self.dimensions
+        return (nx < 0 or ny < 0 or nx >= width or ny >= height)
+
+    
+    def on_walls(self, segment):
+        '''Checks how many arena boundaries body segment is adjacent to
+
+        Parameters:
+        segment -- (x, y) coordinates of body segment
+        '''
+        x, y = segment
+        x_lim, y_lim = self.dimensions
+        walls = 0
+        if x == 1: walls += 1
+        if x == x_lim-1: walls += 1
+        if y == 1: walls += 1
+        if y == ylim-1: walls += 1
+        return walls
 
 
     def rank_moves(self):
